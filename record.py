731219ed9e7e79cc1b03ec06565a63da2d0ba19d
Davi Francisco <franciscodavi327@gmail.com>
2026-09-26 08:26:32 -0300

    feat: gerar multiplas respostas com base em perguntas

--- record.py
+++ record.py
@@ -1,21 +1,23 @@
-import csv
+import json
 
 from dotenv import load_dotenv
 from langchain.agents import create_agent
 from langchain.tools import BaseTool, tool
 from langchain_chroma import Chroma
+from langchain_core.messages import HumanMessage
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from langchain_huggingface.embeddings import HuggingFaceEmbeddings
+from langgraph.graph.state import CompiledStateGraph
+from tqdm import tqdm
 
 
 load_dotenv()
 
 
-def get_questions(path: str) -> list[list[str]]:
-    with open(path, encoding='utf-8') as f:
-        questions = csv.reader(f, delimiter=';')
-        next(questions)
-        return list(questions)
+def get_questions() -> list[str]:
+    with open('./questions.json', encoding='utf-8') as f:
+        questions = json.load(f)
+        return [question['pergunta'] for question in questions]
 
 
 def build_search_tool(vector_store: Chroma) -> BaseTool:
@@ -32,11 +34,8 @@ def build_search_tool(vector_store: Chroma) -> BaseTool:
             String com contendo o conteúdo dos materiais encontrados e a fonte
         """
 
-        result = vector_store.similarity_search(
-            query,
-            # filter={'discipline': None},
-            k=10,
-        )
+        # Filtrar por disciplina
+        result = vector_store.similarity_search(query, k=10)
 
         if not result:
             return 'Nenhum resultado encontrado nos documentos indexados.'
@@ -48,9 +47,21 @@ def build_search_tool(vector_store: Chroma) -> BaseTool:
     return search_documentation
 
 
+def generate_answer(question: str, agent: CompiledStateGraph) -> str:
+    result = agent.invoke({'messages': [HumanMessage(question)]})
+    return result['messages'][-1].content
+
+
+def save_answer(answer: str):
+    with open('./answers.txt', 'a+', encoding='utf-8') as f:
+        f.write(answer + '\n\n---\n\n')
+
+
 def main():
     # if (pinecone_api_key := os.getenv('PINECONE_API_KEY')) is None:
     #     raise RuntimeError('A variável de ambiente "PINECONE_API_KEY" está vazia ou não foi definida')
+
+    SYSTEM_PROMPT = 'Você é um assistente que responde perguntas usando os materiais de estudo indexados. Use a ferramenta search_documentation para buscar contexto antes de responder. Se a resposta não estiver nos documentos, diga isso claramente em vez de inventar'
 
     embeddings = HuggingFaceEmbeddings(
         model_name='Qwen/Qwen3-Embedding-0.6B', encode_kwargs={'normalize_embeddings': True}
@@ -65,13 +76,12 @@ def main():
     agent = create_agent(
         model=llm,
         tools=[build_search_tool(vector_store)],
-        system_prompt='Você é um assistente que responde perguntas usando os materiais de estudo indexados. Use a ferramenta search_documentation para buscar contexto antes de responder. Se a resposta não estiver nos documentos, diga isso claramente em vez de inventar',
+        system_prompt=SYSTEM_PROMPT,
     )
 
-    result = agent.invoke(
-        {'messages': [{'role': 'user', 'content': 'Qual a diferença entre Docker Máquinas virtuais?'}]}
-    )
-    print(f'\nResposta: {result["messages"][-1].content}')
+    for question in tqdm(get_questions(), desc='Gerando respostas', unit='pergunta'):
+        answer = generate_answer(question, agent)
+        save_answer(answer)
 
 
 if __name__ == '__main__':
